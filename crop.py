eaaff5148f853cd9ddd8fea70c1e4533f98123da
JerlinJR <infodotjerlin@gmail.com>
2022-06-29 18:47:59 +0530

    Automation of croping

--- crop.py
+++ crop.py
@@ -1,7 +1,19 @@
+import os
 import moviepy.editor as mpy
 from moviepy.video.fx.all import crop
 
-clip = mpy.VideoFileClip("/Path to the video/")
-(w, h) = clip.size
-cropped_clip = crop(clip, width=600, height=5000, x_center=w/2, y_center=h/2)
-cropped_clip.write_videofile('/Path of video to be saved/')
+# Specify the path correctly
+path = '/home/lenovo/Videos/Edit/'
+# path = '123/'
+print("Starting....")
+
+for filename in os.listdir(path):
+        clip = mpy.VideoFileClip(path+filename)
+        (w, h) = clip.size
+        cropped_clip = crop(clip, width=600, height=5000, x_center=w/2, y_center=h/2)
+        cropped_clip.write_videofile('/home/lenovo/Videos/sample/'+filename)
+
+
+
+
+
