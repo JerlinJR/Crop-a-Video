68611bdf9a90ef2f287030300fc46bcbdd9440a3
cohyun <101383358+cohyun@users.noreply.github.com>
2023-03-20 17:30:35 +0900

    Update crop.py
    Hi Thanks for the excellent job. 
    I found some error, and changing code like how i recommeded below could solve the error.
    
    I added the codec version in the Write_videofile() method.
    Also in output_video path, adding mp4 file name could make code run easier. 
    
    Thanks

--- crop.py
+++ crop.py
@@ -8,7 +8,7 @@ path = '/home/lenovo/Videos/Youtube/ToCrop/'
 
 
 #path of the video file to be saved after cropping
-output_video = '/home/lenovo/Videos/Youtube/sample/'
+output_video = '/home/lenovo/Videos/Youtube/sample/crop_video.mp4'
 
 
 for filename in os.listdir(path):
@@ -16,7 +16,7 @@ for filename in os.listdir(path):
         clip = mpy.VideoFileClip(path+filename)
         (w, h) = clip.size
         cropped_clip = crop(clip, width=600, height=5000, x_center=w/2, y_center=h/2)
-        cropped_clip.write_videofile(output_video+filename)
+        cropped_clip.write_videofile(output_video+filename,codec="libx264")
 
 
 
